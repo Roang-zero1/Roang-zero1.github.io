3072ccf67f02e204e645cc34ed5b402ad45b88a5
Lucas Brandstaetter <lucas@brandstaetter.tech>
2023-08-22 09:04:06 +0200

    Update dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -115,7 +115,6 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-postcss`,
-    `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
